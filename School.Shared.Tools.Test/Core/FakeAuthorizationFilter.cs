4a5223f55cfae631668d3ebb2e4381b40a4e93da
Nichlas Hoberg <nichlashoberg@gmail.com>
2024-09-07 17:55:26 +0200

    feat: added test tools

---
+++ School.Shared.Tools.Test/Core/FakeAuthorizationFilter.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace School.Shared.Tools.Test.Core;
+
+public class FakeAuthorizationFilter : IAsyncActionFilter
+{
+    private readonly ClaimsPrincipal _principal;
+
+    public FakeAuthorizationFilter(ClaimsPrincipal? principal = null)
+    {
+        _principal = principal ?? new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+        {
+            new("iid", Guid.NewGuid().ToString()),
+            new("uid", Guid.NewGuid().ToString()),
+            new("sub", "Tester"),
+            new("systems:website:role", "Admin"),
+            new("systems:game:role", "Admin"),
+        }));
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        context.HttpContext.User = _principal;
+
+        await next();
+    }
+}
---
+++ School.Shared.Tools.Test/Core/IntegrationTestAuthorizationHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using School.Shared.Core.Authentication.Claims;
+
+namespace School.Shared.Tools.Test.Core;
+
+public class IntegrationTestAuthorizationHandler : AuthorizationHandler<ClaimRequirement>
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ClaimRequirement requirement)
+    {
+        foreach (var authorizationRequirement in context.PendingRequirements.ToList())
+        {
+            context.Succeed(authorizationRequirement);
+        }
+
+        return Task.CompletedTask;
+    }
+}
